8495fb8a434492af3f816c77b0a8ea1b227fd791
daspryJ <100787407+daspryJ@users.noreply.github.com>
2022-03-02 16:54:07 +0100

    Create divinedaolibrary_shortcut.js
    Allow the use of next/previous keyboard arrows to navigate to next/previous chapter on DivineDaoLibrary

---
+++ divinedaolibrary_shortcut.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         divinedaolibrary shortcut
+// @namespace    https://github.com/daspryJ/Divinedaolibrary-shortcut
+// @version      0.1
+// @description  next/prev chapter shortcuts
+// @author       daspryJ
+// @match        https://www.divinedaolibrary.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=divinedaolibrary.com
+// @grant        none
+// ==/UserScript==
+
+document.onkeyup = function(e) { // key pressed
+    if(document.activeElement.nodeName === "INPUT"
+    || document.activeElement.nodeName === "TEXTAREA") {
+        return; // abort if focusing input box
+    }
+
+    var elems = document.getElementsByTagName("a"),
+        links = {};
+
+    for(var i = elems.length-1; i >= 0; i--) { // filter link elements
+        var elem = elems[i];
+        if(elem.rel === "prev") { // add prev to links object
+            links.prev = elem;
+        } else if(elem.rel === "next") { // ad next to links object
+            links.next = elem;
+        }
+    }
+
+    if(e.keyCode === 37) { // left key
+        location.href = links.prev.href;
+    } else if(e.keyCode === 39) { // right key
+        location.href = links.next.href;
+    }
+};
